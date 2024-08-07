a930e398bb96f13515fc905ed449d98b18f714db
CHAITANYA Laxman Gudalamani <125981808+LGChaitanya@users.noreply.github.com>
2024-08-07 19:19:01 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
 export const URL="https://microservices-based-blog-app-main-backend.onrender.com"
-export const IF="http://localhost:5000/images/"
+export const IF="https://microservices-based-blog-app-main-backend.onrender.com"
